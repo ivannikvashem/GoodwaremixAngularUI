ab32cc489f47c457ca2b6d2b937bf0702c0f9c3c
Ivan <meshavkin_is@ekt.sbat.ru>
2024-07-08 17:43:25 +0500

    possible suppliers fix

--- src/app/users/user-details/user-details.component.ts
+++ src/app/users/user-details/user-details.component.ts
@@ -36,6 +36,8 @@ export class UserDetailsComponent implements OnInit {
     token: ['']
   });
 
+  linkedSuppliers:any[] = [];
+
   constructor(private fb: FormBuilder,
               private api: ApiClient,
               private router: Router,
@@ -48,11 +50,12 @@ export class UserDetailsComponent implements OnInit {
   ngOnInit(): void {
     if (this.data?.id) {
       this.userForm.setValue(this.data);
+      this.linkedSuppliers = this.data.linkedSuppliers;
     }
 
     if (this.dss.getSupplierList().length == 0) {
       this.supplierDS.loadAutocompleteData("", 0 ,100, "supplierName", "asc").subscribe( (r:any) => {
-        this.supplierList = r.body.data
+        this.supplierList = r;
         this.dss.setSupplierList(this.supplierList)
       });
     } else {
@@ -65,9 +68,9 @@ export class UserDetailsComponent implements OnInit {
   }
 
   removeSupplierFromUser(item: any): void {
-    const index = this.userForm.value.linkedSuppliers?.indexOf(item);
+    const index = this.linkedSuppliers?.indexOf(item);
     if (typeof(index) == "number" && index >= 0) {
-      this.userForm.value.linkedSuppliers?.splice(index, 1);
+      this.linkedSuppliers?.splice(index, 1);
     }
   }
 
@@ -75,17 +78,19 @@ export class UserDetailsComponent implements OnInit {
     const value = (event.option.value.id || '').trim();
     const idx = this.userForm.value.linkedSuppliers.find((x:any) => x.id == value);
     if (!idx) {
-      this.userForm.value.linkedSuppliers.push({id: event.option.value.id, name: event.option.viewValue});
+      this.linkedSuppliers.push({id: event.option.value.id, name: event.option.viewValue});
     }
-    this.suppliersSearchInput.nativeElement.value = '';
-    this.suppliersSearchCtrl.setValue(null);
+    /*this.suppliersSearchInput.nativeElement.value = '';
+    this.suppliersSearchCtrl.setValue(null);*/
+    console.log(this.userForm.value.linkedSuppliers)
   }
 
   onSubmit(): void {
     if (!this.data?.id) {
-      console.log("submitting " + this.userForm.value.username + " by ID " + this.data?.id + " with data: " + JSON.stringify(this.userForm.value));
+      //console.log("submitting " + this.userForm.value.username + " by ID " + this.data?.id + " with data: " + JSON.stringify(this.userForm.value));
       delete this.userForm.value.id;
       delete this.userForm.value.lastLogin;
+      this.userForm.value.linkedSuppliers = this.linkedSuppliers;
       this.api.postRequest('users',this.userForm.value).subscribe(() => {
         this.dialogRef.close(this.userForm.value)
       },error => {
@@ -104,9 +109,9 @@ export class UserDetailsComponent implements OnInit {
 
   selectAll() {
     for (let i of this.supplierList) {
-      const idx = this.userForm.value.linkedSuppliers.find((x:any) => x.id == i.id);
+      const idx = this.linkedSuppliers.find((x:any) => x.id == i.id);
       if (!idx) {
-        this.userForm.value.linkedSuppliers.push({id: i.id, name: i.supplierName});
+        this.linkedSuppliers.push({id: i.id, name: i.supplierName});
       }
     }
   }
